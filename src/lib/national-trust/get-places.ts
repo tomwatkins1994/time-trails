fdf05f08e29ac4415833edfdd50bb024b4852380
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-07 23:31:10 +0100

    Add biome

--- src/lib/national-trust/get-places.ts
+++ src/lib/national-trust/get-places.ts
@@ -14,7 +14,7 @@ export type NTPlacesResult = z.infer<typeof NTPlacesResultSchema>;
 export async function getPlaces(): Promise<NTPlacesResult> {
   const response = await fetch('https://www.nationaltrust.org.uk/api/search/places?query=&lat=52&lon=0&milesRadius=1000&maxPlaceResults=1000');
   if (!response.ok) {
-    throw new Error('Network response was not ok');
+    throw new Error('Network response was not ok'); 
   }
   const data = await response.json();
   const parsedData = NTPlacesResultSchema.parse(data);
