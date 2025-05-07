8ba18440d303b7a7794d5df7d257e034e7e821c6
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-08 00:23:50 +0100

    Use MSW to make tests more reliable

--- src/lib/national-trust/get-places.test.ts
+++ src/lib/national-trust/get-places.test.ts
@@ -1,7 +1,16 @@
-import { describe, expect, it } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import { getPlaces } from "./get-places";
+import { nationalTrustMockServer } from "../../mocks/national-trust/server";
 
 describe("getPlaces", () => {
+	beforeAll(() => {
+		nationalTrustMockServer.listen();
+	});
+
+	afterAll(() => {
+		nationalTrustMockServer.close();
+	});
+
 	it("should fetch places data", async () => {
 		const data = await getPlaces();
 		expect(data).toHaveProperty("multiMatch");
