1152551badc190473e299e5a013a099380ca417b
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-09 01:46:43 +0100

    Show places on page

--- src/router.tsx
+++ src/router.tsx
@@ -1,34 +1,29 @@
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
-import { QueryClient } from '@tanstack/react-query'
-import { routerWithQueryClient } from '@tanstack/react-router-with-query'
-import * as TanstackQuery from "./providers/query-client-provider"
-import { routeTree } from './routeTree.gen'
+import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+import { routerWithQueryClient } from "@tanstack/react-router-with-query";
+import * as TanstackQuery from "./providers/query-client-provider";
+import { routeTree } from "./routeTree.gen";
+import { QueryClient } from "@tanstack/react-query";
 
 export function createRouter() {
-  const queryClient = new QueryClient()
+	const queryContext = TanstackQuery.getContext();
 
-  const router = routerWithQueryClient(
-    createTanStackRouter({
-      routeTree,
-      context: {
-        ...TanstackQuery.getContext(),
-      },
-      scrollRestoration: true,
-      defaultPreloadStaleTime: 0,
-      Wrap: (props: { children: React.ReactNode }) => {
-        return (
-          <TanstackQuery.Provider>{props.children}</TanstackQuery.Provider>
-        );
-      },
-    }),
-    TanstackQuery.getContext().queryClient
-  );
+	const router = createTanStackRouter({
+		routeTree,
+		context: {
+			...queryContext,
+		},
+		scrollRestoration: true,
+		defaultPreloadStaleTime: 0,
+		Wrap: (props: { children: React.ReactNode }) => {
+			return <TanstackQuery.Provider>{props.children}</TanstackQuery.Provider>;
+		},
+	});
 
-  return router
+	return routerWithQueryClient(router, queryContext.queryClient);
 }
 
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: ReturnType<typeof createRouter>
-  }
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: ReturnType<typeof createRouter>;
+	}
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,47 +1,29 @@
-import * as fs from 'node:fs'
-import { createFileRoute, useRouter } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/react-start'
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { useTRPC } from "../trpc/react";
+import { useQuery } from "@tanstack/react-query";
 
-const filePath = 'count.txt'
-
-async function readCount() {
-  return Number.parseInt(
-    await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
-  )
-}
-
-const getCount = createServerFn({
-  method: 'GET',
-}).handler(() => {
-  return readCount()
-})
-
-const updateCount = createServerFn({ method: 'POST' })
-  .validator((d: number) => d)
-  .handler(async ({ data }) => {
-    const count = await readCount()
-    await fs.promises.writeFile(filePath, `${count + data}`)
-  })
-
-export const Route = createFileRoute('/')({
-  component: Home,
-  loader: async () => await getCount(),
-})
+export const Route = createFileRoute("/")({
+	component: Home,
+	loader: async ({ context }) => {
+		await context.queryClient.prefetchQuery(
+			context.trpc.places.list.queryOptions(),
+		);
+	},
+});
 
 function Home() {
-  const router = useRouter()
-  const state = Route.useLoaderData()
+	const trpc = useTRPC();
+	const { data: places, isLoading } = useQuery(trpc.places.list.queryOptions());
 
-  return (
-    <button
-      type="button"
-      onClick={() => {
-        updateCount({ data: 1 }).then(() => {
-          router.invalidate()
-        })
-      }}
-    >
-      Add 1 to {state}?
-    </button>
-  )
+	if (isLoading) {
+		return "Loading...";
+	}
+
+	return (
+		<>
+			{places?.map((place) => (
+				<div key={place.id}>{place.name}</div>
+			))}
+		</>
+	);
 }
