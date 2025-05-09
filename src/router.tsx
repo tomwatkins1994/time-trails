c5f6b7a6f52f7dcc760f1d67d0911009e7fe2bf9
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-09 01:06:16 +0100

    Setup tprc react query

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,28 @@
 import { createRouter as createTanStackRouter } from '@tanstack/react-router'
+import { QueryClient } from '@tanstack/react-query'
+import { routerWithQueryClient } from '@tanstack/react-router-with-query'
+import * as TanstackQuery from "./providers/query-client-provider"
 import { routeTree } from './routeTree.gen'
 
 export function createRouter() {
-  const router = createTanStackRouter({
-    routeTree,
-    scrollRestoration: true,
-  })
+  const queryClient = new QueryClient()
+
+  const router = routerWithQueryClient(
+    createTanStackRouter({
+      routeTree,
+      context: {
+        ...TanstackQuery.getContext(),
+      },
+      scrollRestoration: true,
+      defaultPreloadStaleTime: 0,
+      Wrap: (props: { children: React.ReactNode }) => {
+        return (
+          <TanstackQuery.Provider>{props.children}</TanstackQuery.Provider>
+        );
+      },
+    }),
+    TanstackQuery.getContext().queryClient
+  );
 
   return router
 }
