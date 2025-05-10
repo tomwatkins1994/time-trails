21478a035116fcf59bee0c38188bd23525829f50
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-10 02:00:07 +0100

    Move trpc lofic into router

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,49 @@
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { routerWithQueryClient } from "@tanstack/react-router-with-query";
-import * as TanstackQuery from "./providers/query-client-provider";
+import { QueryClient } from "@tanstack/react-query";
+import { createTRPCClient, httpBatchStreamLink } from "@trpc/client";
+import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
+import superjson from "superjson";
+
 import { routeTree } from "./routeTree.gen";
+import type { TRPCRouter } from "./trpc/router";
+import { TRPCProvider } from "./trpc/react";
 
 export function createRouter() {
-	const queryContext = TanstackQuery.getContext();
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			dehydrate: { serializeData: superjson.serialize },
+			hydrate: { deserializeData: superjson.deserialize },
+		},
+	});
+
+	function getUrl() {
+		const base = (() => {
+			if (typeof window !== "undefined") return "";
+			return `http://localhost:${process.env.PORT ?? 3000}`;
+		})();
+		return `${base}/api/trpc`;
+	}
+
+	const trpcClient = createTRPCClient<TRPCRouter>({
+		links: [
+			httpBatchStreamLink({
+				transformer: superjson,
+				url: getUrl(),
+			}),
+		],
+	});
+
+	const serverHelpers = createTRPCOptionsProxy({
+		client: trpcClient,
+		queryClient: queryClient,
+	});
 
 	const router = createTanStackRouter({
 		routeTree,
 		context: {
-			...queryContext,
+			queryClient,
+			trpc: serverHelpers,
 		},
 		defaultPreload: "intent",
 		// defaultErrorComponent: DefaultCatchBoundary,
@@ -17,11 +51,15 @@ export function createRouter() {
 		scrollRestoration: true,
 		defaultPreloadStaleTime: 0,
 		Wrap: (props: { children: React.ReactNode }) => {
-			return <TanstackQuery.Provider>{props.children}</TanstackQuery.Provider>;
+			return (
+				<TRPCProvider trpcClient={trpcClient} queryClient={queryClient}>
+					{props.children}
+				</TRPCProvider>
+			);
 		},
 	});
 
-	return routerWithQueryClient(router, queryContext.queryClient);
+	return routerWithQueryClient(router, queryClient);
 }
 
 declare module "@tanstack/react-router" {
