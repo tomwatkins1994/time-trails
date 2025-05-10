aa00c186666075f9c2c8041a758e5a3171655601
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-11 00:00:01 +0100

    Upsert places function

---
+++ src/lib/national-trust/upsert-places.ts
@@ -0,0 +1,32 @@
+import { db } from "@/db";
+import { getPlaces } from "./get-places";
+import { places } from "@/db/schema/places";
+import { sql } from "drizzle-orm";
+
+export async function upsertPlaces() {
+	const ntPlaces = (await getPlaces()).multiMatch.results.map((place) => ({
+		name: place.title,
+		description: place.description,
+		town: place.town,
+		county: place.county,
+		imageUrl: place.imageUrl,
+		managedBy: "NATIONAL_TRUST" as const,
+		managerId: place.id.value,
+		managerWebsiteUrl: place.websiteUrl,
+	}));
+
+	await db
+		.insert(places)
+		.values(ntPlaces)
+		.onConflictDoUpdate({
+			target: [places.managedBy, places.managerId],
+			set: {
+				name: sql`exlucded.name`,
+				description: sql`exlucded.description`,
+				town: sql`exlucded.town`,
+				county: sql`exlucded.county`,
+				imageUrl: sql`exlucded.imageUrl`,
+				managerWebsiteUrl: sql`exlucded.websiteUrl`,
+			},
+		});
+}
