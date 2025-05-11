3fd6e203b8bb6b0c83c98acf8771cc9df1f5212f
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-11 01:09:09 +0100

    Add tsconfig paths

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vitest/config";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	test: {
 		setupFiles: ["./test/setup-test-db.ts"],
 	},
+	plugins: [
+		tsConfigPaths({
+			projects: ["./tsconfig.json"],
+		}),
+	],
 });
