241e005be2028cd2a4daf128f50a015dbd406ff7
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-11 01:27:38 +0100

    Add unique constraint

--- src/db/schema/places.ts
+++ src/db/schema/places.ts
@@ -1,15 +1,19 @@
-import { pgEnum, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, unique, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const placeManager = pgEnum("place_manager", ["NATIONAL_TRUST"]);
 
-export const places = pgTable("places", {
-	id: uuid().primaryKey().defaultRandom(),
-	name: varchar({ length: 255 }).notNull(),
-	description: varchar({ length: 255 }).notNull(),
-	town: varchar(),
-	county: varchar(),
-	imageUrl: varchar(),
-	managedBy: placeManager(),
-	managerId: varchar(),
-	managerWebsiteUrl: varchar(),
-});
+export const places = pgTable(
+	"places",
+	{
+		id: uuid().primaryKey().defaultRandom(),
+		name: varchar({ length: 255 }).notNull(),
+		description: varchar({ length: 255 }).notNull(),
+		town: varchar(),
+		county: varchar(),
+		imageUrl: varchar(),
+		managedBy: placeManager(),
+		managerId: varchar(),
+		managerWebsiteUrl: varchar(),
+	},
+	(t) => [unique().on(t.managedBy, t.managerId).nullsNotDistinct()],
+);
