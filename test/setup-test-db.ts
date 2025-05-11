5c6665f4043cb54ce641e23fbbe0ac8e0f3bf255
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-11 01:33:08 +0100

    Use getConnectionUri

--- test/setup-test-db.ts
+++ test/setup-test-db.ts
@@ -1,24 +1,19 @@
-import { setupDb } from "@/db";
 import { PostgreSqlContainer } from "@testcontainers/postgresql";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import path from "node:path";
 import { afterAll, beforeAll, vi } from "vitest";
-
-const POSTGRES_USER = "test";
-const POSTGRES_PASSWORD = "test";
-const POSTGRES_DB = "test";
+import { setupDb } from "@/db";
 
 const container = await new PostgreSqlContainer()
 	.withEnvironment({
-		POSTGRES_USER: POSTGRES_USER,
-		POSTGRES_PASSWORD: POSTGRES_PASSWORD,
-		POSTGRES_DB: POSTGRES_DB,
+		POSTGRES_USER: "test",
+		POSTGRES_PASSWORD: "test",
+		POSTGRES_DB: "test",
 	})
 	.withExposedPorts(5432)
 	.start();
 
-const connectionString = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${container.getHost()}:${container.getFirstMappedPort()}/${POSTGRES_DB}`;
-const db = setupDb(connectionString);
+const db = setupDb(container.getConnectionUri());
 
 vi.doMock("@/db", async (importOriginal) => {
 	return {
@@ -28,9 +23,8 @@ vi.doMock("@/db", async (importOriginal) => {
 });
 
 beforeAll(async () => {
-	const migrationPath = path.join(process.cwd(), "drizzle");
 	await migrate(db, {
-		migrationsFolder: migrationPath,
+		migrationsFolder: path.join(process.cwd(), "drizzle"),
 	});
 });
 
