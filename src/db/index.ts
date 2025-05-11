63409ef15b8ac14e19823cdf3ee06ab881921018
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-11 01:17:28 +0100

    New function and snake case

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,6 +3,10 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { z } from "zod";
 import * as schema from "./schema";
 
+export function setupDb(url: string) {
+	return drizzle(DATABASE_URL, { schema, casing: "snake_case" });
+}
+
 const DATABASE_URL = z.string().parse(process.env.DATABASE_URL);
 
-export const db = drizzle(DATABASE_URL, { schema });
+export const db = setupDb(DATABASE_URL);
