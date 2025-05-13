b63cb307d738316cf08c277d49e311f9ee2930b5
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-14 00:06:29 +0100

    Simple infinite query

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,38 +1,75 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Router } from "@tanstack/react-router";
 import { useTRPC } from "../trpc/react";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { PhotoCard } from "@/components/photo-card";
 import { CardLayoutGrid } from "@/components/card-layout-grid";
+import { z } from "zod";
 
 export const Route = createFileRoute("/")({
 	component: Home,
-	loader: async ({ context }) => {
-		await context.queryClient.prefetchQuery(
-			context.trpc.places.list.queryOptions(),
+	validateSearch: z.object({
+		cursor: z.string().optional(),
+	}),
+	loaderDeps: ({ search }) => ({
+		cursor: search.cursor,
+	}),
+	loader: async ({ context, deps }) => {
+		await context.queryClient.prefetchInfiniteQuery(
+			context.trpc.places.infiniteList.infiniteQueryOptions({
+				cursor: deps.cursor ?? null,
+			}),
 		);
 	},
 });
 
 function Home() {
 	const trpc = useTRPC();
-	const { data: places, isLoading } = useQuery(trpc.places.list.queryOptions());
+	const { cursor } = Route.useLoaderDeps();
+	const {
+		data: places,
+		isLoading,
+		fetchNextPage,
+		hasNextPage,
+	} = useInfiniteQuery(
+		trpc.places.infiniteList.infiniteQueryOptions(
+			{ cursor: cursor ?? null },
+			{
+				getNextPageParam: (lastPage) => lastPage.nextCursor,
+			},
+		),
+	);
 
 	if (isLoading) {
 		return "Loading...";
 	}
 
 	return (
-		<CardLayoutGrid>
-			{places?.map((place) => (
-				<PhotoCard
-					key={place.id}
-					imageUrl={place.imageUrl}
-					title={place.name}
-					subTitle={[place.town, place.county].filter(Boolean).join(", ")}
-					description={place.description}
-					managedBy={place.managedBy}
-				/>
-			))}
-		</CardLayoutGrid>
+		<>
+			<CardLayoutGrid>
+				{places?.pages.map((page) =>
+					page.items.map((place) => (
+						<PhotoCard
+							key={place.id}
+							imageUrl={place.imageUrl}
+							title={place.name}
+							subTitle={[place.town, place.county].filter(Boolean).join(", ")}
+							description={place.description}
+							managedBy={place.managedBy}
+						/>
+					)),
+				)}
+			</CardLayoutGrid>
+			{hasNextPage ? (
+				<button
+					type="button"
+					onClick={() => {
+						console.log("Pressed");
+						fetchNextPage();
+					}}
+				>
+					Fetch next page
+				</button>
+			) : null}
+		</>
 	);
 }
--- src/trpc/router/places.ts
+++ src/trpc/router/places.ts
@@ -1,9 +1,31 @@
+import { z } from "zod";
 import { db } from "../../db";
 import { publicProcedure } from "../init";
 
 export const placesRouter = {
-	list: publicProcedure.query(async () => {
-		const places = await db.query.places.findMany();
-		return places;
-	}),
+	infiniteList: publicProcedure
+		.input(
+			z.object({
+				cursor: z.string().nullable(),
+				limit: z.number().default(10),
+			}),
+		)
+		.query(async ({ input: { cursor, limit } }) => {
+			const places = await db.query.places.findMany({
+				where: (t, { gt }) => (cursor ? gt(t.id, cursor) : undefined),
+				orderBy: (t, { asc }) => asc(t.id),
+				limit: limit + 1,
+			});
+
+			let nextCursor: typeof cursor | undefined = undefined;
+			if (places.length > limit) {
+				const nextPlace = places.pop();
+				nextCursor = nextPlace?.id;
+			}
+
+			return {
+				items: places,
+				nextCursor,
+			};
+		}),
 };
