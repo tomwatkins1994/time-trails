cd557f43cf6de27f64755d5639164bf4dfe481a4
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-16 00:17:55 +0100

    Add lato as base font

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -40,7 +40,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 			{ rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "" },
 			{
 				rel: "stylesheet",
-				href: "https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&display=swap",
+				href: "https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap",
 			},
 		],
 	}),
