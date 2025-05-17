d79b8d23b7610545440aaf30484fb412ffb883fc
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-17 15:44:03 +0100

    Test places infinite list

---
+++ src/trpc/router/places.test.ts
@@ -0,0 +1,44 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import { seed } from "drizzle-seed";
+import { createCaller } from ".";
+import { db } from "@/db";
+import { places } from "@/db/schema";
+
+describe("infiniteList", () => {
+	const ctx = {};
+	const caller = createCaller(ctx);
+
+	const pages = 5;
+	const limit = 10;
+
+	beforeAll(async () => {
+		await seed(db, { places }, { count: pages * limit });
+	});
+
+	it("should get a page of data with the correct size and with a next cursor", async () => {
+		const { items, nextCursor } = await caller.places.infiniteList({
+			cursor: null,
+			limit,
+		});
+		expect(items.length).toBe(limit);
+		expect(nextCursor).toBeTruthy();
+	});
+
+	it("should get all pages of data", async () => {
+		let cursor: string | null = null;
+		for (let i = 1; i <= pages; i++) {
+			const { items, nextCursor } = await caller.places.infiniteList({
+				cursor,
+				limit,
+			});
+			expect(items.length).toBe(limit);
+			if (i > 1) {
+				expect(items[0]?.id).toBe(cursor);
+			}
+			if (i === pages) {
+				expect(nextCursor).toBe(null);
+			}
+			cursor = nextCursor;
+		}
+	});
+});
