e2db2f0c0f47f2aa6932f3cf7b61bf195e7af91f
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-17 15:08:56 +0100

    Remove unneeded import

--- test/setup-test-db.ts
+++ test/setup-test-db.ts
@@ -1,7 +1,7 @@
 import { PostgreSqlContainer } from "@testcontainers/postgresql";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import path from "node:path";
-import { afterAll, beforeAll, vi } from "vitest";
+import { afterAll, vi } from "vitest";
 import { setupDb } from "@/db";
 
 const container = await new PostgreSqlContainer("postgres:17.5").start();
