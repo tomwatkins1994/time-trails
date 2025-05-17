3b00132e4bb61b37a7c67433d8cbf1fcaf8da933
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-17 15:13:25 +0100

    Remove unneed deletes

--- src/lib/national-trust/upsert-places.test.ts
+++ src/lib/national-trust/upsert-places.test.ts
@@ -7,20 +7,15 @@ import { places } from "@/db/schema";
 describe("upsertPlaces", () => {
 	beforeAll(() => {
 		nationalTrustMockServer.listen();
-		db.delete(places);
 	});
 
 	afterAll(() => {
 		nationalTrustMockServer.close();
 	});
 
-	afterEach(() => {
-		db.delete(places);
-	});
-
 	it("Should get places data and add it to the database", async () => {
+		expect(await db.$count(places)).toBe(0);
 		await upsertPlaces();
-
 		expect(await db.$count(places)).toBeGreaterThan(0);
 	});
 });
