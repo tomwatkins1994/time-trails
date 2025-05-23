a2f4850952c8ef17ea4c2f346cfc271768a1d78c
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-23 22:29:39 +0100

    Add trpcClient

--- src/router.tsx
+++ src/router.tsx
@@ -1,22 +1,12 @@
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { routerWithQueryClient } from "@tanstack/react-router-with-query";
 import { QueryClient } from "@tanstack/react-query";
-import { createTRPCClient, httpBatchStreamLink } from "@trpc/client";
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
 import superjson from "superjson";
 
 import { routeTree } from "./routeTree.gen";
-import type { TRPCRouter } from "./trpc/router";
-import { TRPCProvider } from "./trpc/react";
-
-function getUrl() {
-	const base = (() => {
-		if (typeof window !== "undefined") return "";
-		if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-		return `http://localhost:${process.env.PORT ?? 3000}`;
-	})();
-	return `${base}/api/trpc`;
-}
+import { TRPCProvider } from "@/trpc/react";
+import { trpcClient } from "@/trpc/client";
 
 export function createRouter() {
 	const queryClient = new QueryClient({
@@ -24,15 +14,6 @@ export function createRouter() {
 			dehydrate: { serializeData: superjson.serialize },
 			hydrate: { deserializeData: superjson.deserialize },
 		},
-	});
-
-	const trpcClient = createTRPCClient<TRPCRouter>({
-		links: [
-			httpBatchStreamLink({
-				transformer: superjson,
-				url: getUrl(),
-			}),
-		],
 	});
 
 	const serverHelpers = createTRPCOptionsProxy({
---
+++ src/trpc/client.ts
@@ -0,0 +1,21 @@
+import { createTRPCClient, httpBatchStreamLink } from "@trpc/client";
+import type { TRPCRouter } from "./router";
+import superjson from "superjson";
+
+function getUrl() {
+	const base = (() => {
+		if (typeof window !== "undefined") return "";
+		if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+		return `http://localhost:${process.env.PORT ?? 3000}`;
+	})();
+	return `${base}/api/trpc`;
+}
+
+export const trpcClient = createTRPCClient<TRPCRouter>({
+	links: [
+		httpBatchStreamLink({
+			transformer: superjson,
+			url: getUrl(),
+		}),
+	],
+});
