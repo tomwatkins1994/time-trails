f456760cda540e8bd391dbf6a2a6cb58781c688d
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-24 23:34:20 +0100

    Search box styling

--- src/components/search-box.tsx
+++ src/components/search-box.tsx
@@ -1,19 +1,33 @@
 import { SearchIcon } from "lucide-react";
-import { useState } from "react";
+import { type ComponentProps, useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { cn } from "@/lib/utils";
 
-export interface SearchBoxProps {
+export interface SearchBoxProps
+	extends Omit<ComponentProps<"div">, "onSubmit"> {
 	initialValue: string | undefined;
 	onSubmit: (value: string) => void | Promise<void>;
 }
 
-export function SearchBox({ initialValue, onSubmit }: SearchBoxProps) {
+export function SearchBox({
+	initialValue,
+	onSubmit,
+	className,
+	...props
+}: SearchBoxProps) {
 	const [inputValue, setInputValue] = useState(initialValue || "");
 
 	return (
-		<div className="flex gap-2">
+		<div
+			className={cn(
+				"flex gap-2 bg-white dark:bg-input/30 p-1 rounded-xl shadow-xs border border-input",
+				className,
+			)}
+			{...props}
+		>
 			<Input
+				className="border-none outline-0 focus:ring-0 focus:ring-offset-0 focus-visible:border-none focus-visible:ring-0 shadow-none"
 				placeholder="Search"
 				value={inputValue}
 				onChange={(e) => setInputValue(e.target.value)}
