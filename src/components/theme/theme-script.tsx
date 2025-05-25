27a49bf362e49971ce01db7d27d1e11e39107e0f
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-25 04:52:14 +0100

    Stringify script

--- src/components/theme/theme-script.tsx
+++ src/components/theme/theme-script.tsx
@@ -1,18 +1,24 @@
+function setThemeScript() {
+	try {
+		const theme = localStorage.getItem("theme") || "system";
+		if (
+			theme === "dark" ||
+			(theme === "system" &&
+				window.matchMedia("(prefers-color-scheme: dark)").matches)
+		) {
+			document.documentElement.classList.add("dark");
+		} else {
+			document.documentElement.classList.remove("dark");
+		}
+	} catch (_) {}
+}
+
 export function ThemeScript() {
 	return (
 		<script
 			// biome-ignore lint/security/noDangerouslySetInnerHtml: Needed to avoid FOUC
 			dangerouslySetInnerHTML={{
-				__html: `
-				try {
-					const theme = localStorage.getItem('theme') || "system";
-					if (theme === 'dark' || (theme === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-						document.documentElement.classList.add('dark')
-					} else {
-						document.documentElement.classList.remove('dark')
-					}
-				} catch(_) {}
-                `,
+				__html: `(${setThemeScript.toString()})()`,
 			}}
 		/>
 	);
