90a826c0d817449b9970c8559810b9b413fce687
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-25 14:10:45 +0100

    Handle flicker with CSS

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,29 +1,14 @@
 import { MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "./theme/theme-provider";
 import { Button } from "./ui/button";
-import { useEffect, useLayoutEffect, useState } from "react";
 
 export function ThemeSwitcher() {
 	const { toggleTheme } = useTheme();
 
 	return (
 		<Button className="w-[36px]" onClick={toggleTheme}>
-			<ThemeIcon />
+			<SunIcon className="hidden dark:block" />
+			<MoonIcon className="dark:hidden" />
 		</Button>
 	);
-}
-
-function ThemeIcon() {
-	const [mounted, setMounted] = useState(false);
-	const { theme } = useTheme();
-
-	useLayoutEffect(() => {
-		setMounted(true);
-	}, []);
-
-	if (!mounted) {
-		return null;
-	}
-
-	return theme === "dark" ? <SunIcon /> : <MoonIcon />;
 }
