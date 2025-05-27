ae056d764ae42372022bc7f1a5c60f039ea90216
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-27 23:48:07 +0100

    Installed ts form

---
+++ src/hooks/useAppForm.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { createFormHook, createFormHookContexts } from "@tanstack/react-form";
+
+const { fieldContext, formContext } = createFormHookContexts();
+
+export const { useAppForm } = createFormHook({
+	fieldComponents: {
+		TextField: Input,
+	},
+	formComponents: {
+		SubmitButton: Button,
+	},
+	fieldContext,
+	formContext,
+});
