006bbe92b5f304db31d421b7d77358b1dbd0c8cc
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-28 22:37:31 +0100

    Add cookie handling

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { reactStartCookies } from "better-auth/react-start";
 import { db } from "@/db";
 
 export const auth = betterAuth({
@@ -9,4 +10,5 @@ export const auth = betterAuth({
 	emailAndPassword: {
 		enabled: true,
 	},
+	plugins: [reactStartCookies()],
 });
