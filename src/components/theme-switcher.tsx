6c15fdb2657b8fe1f713526c6f2f89b9235cadbd
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-29 23:59:01 +0100

    Changed theme switcher to toggle

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,14 +1,36 @@
-import { MoonIcon, SunIcon } from "lucide-react";
+import { MonitorIcon, MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "./theme/theme-provider";
-import { Button } from "./ui/button";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 
 export function ThemeSwitcher() {
-	const { toggleTheme } = useTheme();
+	const { setTheme, theme } = useTheme();
 
 	return (
-		<Button onClick={toggleTheme}>
-			<SunIcon className="hidden dark:block" />
-			<MoonIcon className="dark:hidden" />
-		</Button>
+		<ToggleGroup type="single" variant="outline" defaultValue={theme}>
+			<ToggleGroupItem
+				value="system"
+				size="sm"
+				aria-label="Toggle system"
+				onClick={() => setTheme("system")}
+			>
+				<MonitorIcon size={16} />
+			</ToggleGroupItem>
+			<ToggleGroupItem
+				value="light"
+				size="sm"
+				aria-label="Toggle light"
+				onClick={() => setTheme("light")}
+			>
+				<SunIcon size={16} />
+			</ToggleGroupItem>
+			<ToggleGroupItem
+				value="dark"
+				size="sm"
+				aria-label="Toggle dark"
+				onClick={() => setTheme("dark")}
+			>
+				<MoonIcon size={16} />
+			</ToggleGroupItem>
+		</ToggleGroup>
 	);
 }
