a991eae3977c220e6e29c01689fd880603d62c01
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-29 23:40:28 +0100

    Basic menu

--- src/components/logged-in-user.tsx
+++ src/components/logged-in-user.tsx
@@ -1,6 +1,20 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/react";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuPortal,
+	DropdownMenuSeparator,
+	DropdownMenuShortcut,
+	DropdownMenuSub,
+	DropdownMenuSubContent,
+	DropdownMenuSubTrigger,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export function LoggedInUser() {
 	const trpc = useTRPC();
@@ -12,5 +26,33 @@ export function LoggedInUser() {
 		return <Button>Sign In</Button>;
 	}
 
-	return <>{session.user.name}</>;
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<div>{session.user.name}</div>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="w-56">
+				<DropdownMenuLabel>{session.user.name}</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuGroup>
+					<DropdownMenuItem>
+						Profile
+						<DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						Settings
+						<DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+					</DropdownMenuItem>
+				</DropdownMenuGroup>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem>GitHub</DropdownMenuItem>
+				<DropdownMenuItem>Support</DropdownMenuItem>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem>
+					Log out
+					<DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
 }
