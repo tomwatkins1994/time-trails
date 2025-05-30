5025f2679a05933971083011578c6400516af7cd
tomwatkins1994 <twwatkins@btinternet.com>
2025-05-31 00:30:06 +0100

    Use container query for repsonsive card layout

--- src/components/card-layout-grid.tsx
+++ src/components/card-layout-grid.tsx
@@ -2,8 +2,10 @@ import type { PropsWithChildren } from "react";
 
 export function CardLayoutGrid({ children }: PropsWithChildren) {
 	return (
-		<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-			{children}
+		<div className="@container w-full">
+			<div className=" grid grid-cols-1 @2xl:grid-cols-2 @4xl:grid-cols-3 @6xl:grid-cols-4 gap-4">
+				{children}
+			</div>
 		</div>
 	);
 }
