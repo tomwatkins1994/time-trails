dd15e48bbf39423f0d6097de2dbc301325a61066
tomwatkins1994 <twwatkins@btinternet.com>
2025-06-01 03:12:56 +0100

    Use base filter

--- src/trpc/router/places.ts
+++ src/trpc/router/places.ts
@@ -30,32 +30,24 @@ export const placesRouter = {
 			}),
 		)
 		.query(async ({ input: { cursor, limit, search } }) => {
+			const baseFilter = and(
+				search?.name
+					? ilike(schema.places.name, `%${search.name}%`)
+					: undefined,
+				search?.managedBy && search.managedBy.length > 0
+					? // biome-ignore lint/suspicious/noExplicitAny: Will deal with enum issue later
+						inArray(schema.places.managedBy, search.managedBy as any)
+					: undefined,
+			);
+
 			const places = await db.query.places.findMany({
-				where: (t, { and, gte, ilike, inArray }) =>
-					and(
-						cursor ? gte(t.id, cursor) : undefined,
-						search?.name ? ilike(t.name, `%${search.name}%`) : undefined,
-						search?.managedBy && search.managedBy.length > 0
-							? // biome-ignore lint/suspicious/noExplicitAny: Will deal with enum issue later
-								inArray(t.managedBy, search.managedBy as any)
-							: undefined,
-					),
+				where: (t, { and, gte }) =>
+					and(cursor ? gte(t.id, cursor) : undefined, baseFilter),
 				orderBy: (t, { asc }) => asc(t.id),
 				limit: limit + 1,
 			});
 
-			const count = await db.$count(
-				schema.places,
-				and(
-					search?.name
-						? ilike(schema.places.name, `%${search.name}%`)
-						: undefined,
-					search?.managedBy && search.managedBy.length > 0
-						? // biome-ignore lint/suspicious/noExplicitAny: Will deal with enum issue later
-							inArray(schema.places.managedBy, search.managedBy as any)
-						: undefined,
-				),
-			);
+			const count = await db.$count(schema.places, baseFilter);
 
 			let nextCursor: typeof cursor | null = null;
 			if (places.length > limit) {
