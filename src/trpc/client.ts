75e529fafc22d84d6c0d0c49ab04bc262512d79e
tomwatkins1994 <twwatkins@btinternet.com>
2025-06-03 23:34:31 +0100

    Basic isomorphic client

--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -1,31 +1,81 @@
-import { createTRPCClient, httpBatchStreamLink } from "@trpc/client";
-import type { TRPCRouter } from "./router";
+import {
+	createTRPCClient,
+	httpBatchStreamLink,
+	TRPCClientError,
+} from "@trpc/client";
+import type { OperationResultEnvelope, TRPCLink } from "@trpc/client";
+import { trpcRouter, type TRPCRouter } from "./router";
 import superjson from "superjson";
 import { createIsomorphicFn } from "@tanstack/react-start";
-import { getHeaders } from "@tanstack/react-start/server";
+import { getRequestHeaders } from "@tanstack/react-start/server";
+import { observable } from "@trpc/server/observable";
+import type { AnyTRPCRouter } from "@trpc/server";
+import { auth } from "@/lib/auth";
 
-const getIncomingHeaders = createIsomorphicFn()
-	.client(() => ({}))
-	.server(() => {
-		const headers = getHeaders();
-		return headers;
-	});
-
-function getUrl() {
-	const base = (() => {
-		if (typeof window !== "undefined") return "";
-		if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-		return `http://localhost:${process.env.PORT ?? 3000}`;
-	})();
-	return `${base}/api/trpc`;
-}
+const isomorphicLink = createIsomorphicFn()
+	.client(() =>
+		httpBatchStreamLink({
+			transformer: superjson,
+			url: "/api/trpc",
+		}),
+	)
+	.server(() =>
+		serverLink({
+			router: trpcRouter,
+			createContext: async () => {
+				const headers = new Headers(
+					Object.entries(getRequestHeaders())
+						.filter(([_, value]) => Boolean(value))
+						.map(([name, value]) => [name, value ?? ("" as const)]),
+				);
+				const session = await auth.api.getSession({
+					headers,
+				});
+				return {
+					session,
+				};
+			},
+		}),
+	);
 
 export const trpcClient = createTRPCClient<TRPCRouter>({
-	links: [
-		httpBatchStreamLink({
-			headers: getIncomingHeaders(),
-			transformer: superjson,
-			url: getUrl(),
-		}),
-	],
+	links: [isomorphicLink()],
 });
+
+export function serverLink<
+	TRouter extends AnyTRPCRouter,
+	TContext extends TRouter["_def"]["_config"]["$types"]["ctx"],
+>({
+	router,
+	createContext,
+}: {
+	router: TRouter;
+	createContext: () => TContext | Promise<TContext>;
+}): TRPCLink<TRouter> {
+	return () =>
+		({ op }) =>
+			observable<
+				OperationResultEnvelope<unknown, unknown>,
+				TRPCClientError<TRouter>
+			>((observer) => {
+				async function execute() {
+					const context = { ...op.context, ...(await createContext()) };
+					const caller = router.createCaller(context);
+					try {
+						const procedure = caller[op.path];
+						if (typeof procedure === "function") {
+							const data = await procedure(op.input);
+							observer.next({
+								context,
+								result: { data, type: "data" },
+							});
+						}
+						observer.complete();
+					} catch (err) {
+						observer.error(TRPCClientError.from(err as Error));
+					}
+				}
+
+				void execute();
+			});
+}
