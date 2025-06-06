4d549b9e8bdd9831d17e791662d297df18b30cb5
tomwatkins1994 <twwatkins@btinternet.com>
2025-06-06 01:37:48 +0100

    Setup redis testcontainers

--- test/setup-test-db.ts
+++ test/setup-test-db.ts
@@ -1,11 +1,34 @@
 import { PostgreSqlContainer } from "@testcontainers/postgresql";
+import { RedisContainer } from "@testcontainers/redis";
+import { GenericContainer } from "testcontainers";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import path from "node:path";
 import { afterAll, vi } from "vitest";
 import { setupDb } from "@/db";
 
-const container = await new PostgreSqlContainer("postgres:17.5").start();
-const db = setupDb(container.getConnectionUri());
+const dbContainer = await new PostgreSqlContainer("postgres:17.5").start();
+const redisContainer = await new RedisContainer("redis:8.0.2").start();
+const upstashUrl = redisContainer.getHost();
+const upstashToken = "example_token";
+const upstashContainer = await new GenericContainer(
+	"hiett/serverless-redis-http:latest",
+)
+	.withExposedPorts(8079)
+	.withEnvironment({
+		SRH_MODE: "env",
+		SRH_TOKEN: upstashToken,
+		SRH_CONNECTION_STRING: upstashUrl,
+	})
+	.start();
+
+const db = setupDb({
+	url: dbContainer.getConnectionUri(),
+	cache: {
+		url: upstashUrl,
+		token: upstashToken,
+	},
+});
+
 await migrate(db, {
 	migrationsFolder: path.join(process.cwd(), "drizzle"),
 });
@@ -18,5 +41,7 @@ vi.doMock("@/db", async (importOriginal) => {
 });
 
 afterAll(async () => {
-	container.stop();
+	dbContainer.stop();
+	redisContainer.stop();
+	upstashContainer.stop();
 });
