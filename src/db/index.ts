97cbf395a7a61fcd91250fd2ab7e68c0b1fb68f0
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2025-06-17 02:16:23 +0100

    chore: add basic GitHub actions to run tests in CI (#11)

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,6 +5,8 @@ import { Pool } from "pg";
 import { z } from "zod";
 import * as schema from "./schema";
 
+const isTest = process.env.NODE_ENV === "test";
+
 export interface DBOptions {
 	url: string;
 	cache: {
@@ -30,18 +32,24 @@ export function setupDb({ url, cache }: DBOptions) {
 	};
 }
 
-const { DATABASE_URL, UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN } = z
-	.object({
-		DATABASE_URL: z.string(),
-		UPSTASH_REDIS_REST_URL: z.string(),
-		UPSTASH_REDIS_REST_TOKEN: z.string(),
-	})
-	.parse(process.env);
+export const { db } = (() => {
+	if (process.env.NODE_ENV === "test") {
+		return {} as ReturnType<typeof setupDb>;
+	}
 
-export const { db } = setupDb({
-	url: DATABASE_URL,
-	cache: {
-		url: UPSTASH_REDIS_REST_URL,
-		token: UPSTASH_REDIS_REST_TOKEN,
-	},
-});
+	const { DATABASE_URL, UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN } = z
+		.object({
+			DATABASE_URL: z.string(),
+			UPSTASH_REDIS_REST_URL: z.string(),
+			UPSTASH_REDIS_REST_TOKEN: z.string(),
+		})
+		.parse(process.env);
+
+	return setupDb({
+		url: DATABASE_URL,
+		cache: {
+			url: UPSTASH_REDIS_REST_URL,
+			token: UPSTASH_REDIS_REST_TOKEN,
+		},
+	});
+})();
